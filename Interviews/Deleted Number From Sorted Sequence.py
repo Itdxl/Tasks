7191d26ec2fe86699f2ee98ae283c71b96251abc
Dmitry Shevchenko <logimor@yandex.com>
2023-12-13 10:42:01 +0300

    find deleted number

--- Interviews/Deleted Number From Sorted Sequence.py
+++ Interviews/Deleted Number From Sorted Sequence.py
@@ -1,19 +1,18 @@
 # # на вход дается массив данных в котором числа от одного до N, перемешаны, 1 число удалено
 # # найти удаленное число. Числа изначально шли по порядку и включали все числа от 1 до N
 
-# def Search(array):
-#     expected = 1
-#     for number in array:
-#         if number != expected:
-#             return expected
-#         expected +=1
+def Search(array):
+    expected = 1
+    for number in array:
+        if number != expected:
+            return expected
+        expected +=1
      
 
-# array = list(map(int, input().split()))
+array = list(map(int, input().split()))
 
-# result = Search(array)
-# print(result)
-
+result = Search(array)
+print(result)
 
 
 def Search2(array):
@@ -25,3 +24,20 @@ array = list(map(int, input().split()))
 
 result = Search2(array)
 print(result)
+
+
+def Search3(array):
+    len_second = len(array)
+    len_first = len_second + 1
+
+    summ_first = len_first * (len_first+1) / 2   # S = (n * (n+1)) / 2
+    return int(summ_first - sum(array))
+
+        
+array = list(map(int, input().split()))
+
+result = Search3(array)
+print(result)
+
+
+
